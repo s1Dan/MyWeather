b88e51afdcdbe2e54f7124c616db0b47a768bb48
s1Dan <dannikg@mail.ru>
2022-07-29 18:36:53 +0300

    commit_test_1

--- app/src/main/java/com/s1dan/myweather/fragments/MainFragment.kt
+++ app/src/main/java/com/s1dan/myweather/fragments/MainFragment.kt
@@ -1,26 +1,49 @@
 package com.s1dan.myweather.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.s1dan.myweather.R
-import com.s1dan.myweather.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.s1dan.myweather.databinding.FragmentMainBinding
 import com.s1dan.myweather.databinding.FragmentMainBinding.inflate
 
 class MainFragment : Fragment() {
 
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+   private fun checkPermission() {
+       if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+           pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+       }
+   }
+
 
     companion object {
         @JvmStatic
